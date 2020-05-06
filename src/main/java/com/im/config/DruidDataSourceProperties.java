5fc04532a03a23c754939acc18021431543a9b97
MO_or <hero_orz@163.com>
2020-05-06 18:12:01 +0800

    druid增加防火墙配置，以及一些优化

--- src/main/java/com/im/config/DruidDataSourceProperties.java
+++ src/main/java/com/im/config/DruidDataSourceProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * @program: im
- * @description:
+ * @description: 数据源实体类
  * @author: MO_or
  * @create: 2020-05- 15:16
  **/
@@ -18,40 +18,23 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class DruidDataSourceProperties {
 
     private String username;
-
     private String password;
-
     private String url;
-
     private String driverClassName;
-
     private Integer initialSize;
-
     private Integer maxActive;
-
     private Integer minIdle;
-
     private long maxWait;
-
     private boolean poolPreparedStatements;
-
     private Integer timeBetweenEvictionRunsMillis;
-
     private Integer minEvictableIdleTimeMillis;
-
     private String validationQuery;
-
     private boolean testWhileIdle;
-
     private boolean testOnBorrow;
-
     private boolean testOnReturn;
-
     private String filters;
-
     private Integer maxPoolPreparedStatementPerConnectionSize;
-
     private boolean useGlobalDataSourceStat;
-
     private String connectionProperties;
+
 }
